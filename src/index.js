dbb8120c038a110a9755060574f0a1480d8dc414
Patricia Ong <patricia.ong977@gmail.com>
2022-04-05 12:33:56 -0700

    13:27 time marker

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css'; // For the background
+
+ReactDOM.render(<App />, document.getElementById('root'));
